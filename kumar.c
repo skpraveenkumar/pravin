f08088a860a40378e4128ef954421769a99d5a7b
skpraveenkumar <30384173+skpraveenkumar@users.noreply.github.com>
2017-08-11 14:47:30 +0100

    Update kumar.c

--- kumar.c
+++ kumar.c
@@ -2,16 +2,16 @@
 #include<conio.h>
 void main()
 {
-inta;
+int a;
 printf("enter the number");
 scanf("%d",&a);
 if(a<=0)
 {
 if(a==0)
-printf("you have entered 0");
+printf("/nyou have entered 0");
 else
-printf("you have enterd negative number");
+printf("/nyou have enterd negative number");
 }
 else
-printf("you have entered positive number);
+printf("/nyou have entered positive number");
 }
