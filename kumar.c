cda9235d0db68e7fa6b7cb534ca3f56e6e8558d1
skpraveenkumar <30384173+skpraveenkumar@users.noreply.github.com>
2017-08-11 16:58:57 +0530

    positive, negative or zero

---
+++ kumar.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+inta;
+printf("enter the number");
+scanf("%d",&a);
+if(a<=0)
+{
+if(a==0)
+printf("you have entered 0");
+else
+printf("you have enterd negative number");
+}
+else
+printf("you have entered positive number);
+}
