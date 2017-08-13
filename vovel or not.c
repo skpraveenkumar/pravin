f4411216a86af592f9f98972e21d138bbcec23ac
skpraveenkumar <30384173+skpraveenkumar@users.noreply.github.com>
2017-08-13 11:28:12 +0530

    Update vovel or not.c

--- vovel or not.c
+++ vovel or not.c
@@ -4,4 +4,9 @@ void main()
 char x;
 printf("enter a alphabet");
 scanf("%c",&x);
-  if(x=='a'||x=='A'||x=='e'||x=='E'||x=='i'||x=='I'
+  if(x=='a'||x=='A'||x=='e'||x=='E'||x=='i'||x=='I'||x=='o'||x=='O'||x=='u'||x=='U')
+    printf("The enterd alphabet is a vowel");
+  else
+    printf("The enterd alphabet is a consonent");
+  return(0);
+}
